de301922ed365e67cd0cf72fa8d97c522e17239e
Fogil Zheng <yumin6@ualberta.ca>
2024-11-16 12:40:41 -0700

    added switch, integrated with timer

--- flask_webapp/static/scripts.js
+++ flask_webapp/static/scripts.js
@@ -1,1 +1,8 @@
 // Javascript file 
+$(document).ready(function () {
+    $(".switch").on("click", function () {
+      console.log("Switch clicked"); // Check if this logs
+      $(".toggle-btn").toggleClass("active");
+    });
+  });
+  
