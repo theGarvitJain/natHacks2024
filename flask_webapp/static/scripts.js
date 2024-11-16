79fcff6f86205e3f26390bb6957c13f4805d7f3e
Fogil Zheng <yumin6@ualberta.ca>
2024-11-16 13:32:28 -0700

    changed background colour when focus mode is switched

--- flask_webapp/static/scripts.js
+++ flask_webapp/static/scripts.js
@@ -1,8 +1,52 @@
 // Javascript file 
+let timerInterval;
+
+function updateTime() {
+    fetch('/time')
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('timer').innerText = data.time;
+        });
+}
+
+function startTimer() {
+    fetch('/start', { method: 'POST' })
+        .then(() => {
+            if (!timerInterval) {
+                timerInterval = setInterval(updateTime, 1000);
+            }
+        });
+}
+
+function stopTimer() {
+    fetch('/stop', { method: 'POST' })
+        .then(() => {
+            clearInterval(timerInterval);
+            timerInterval = null;
+        });
+}
+
+function resetTimer() {
+    fetch('/reset', { method: 'POST' })
+        .then(() => {
+            clearInterval(timerInterval);
+            timerInterval = null;
+            updateTime();
+        });
+}
+
 $(document).ready(function () {
     $(".switch").on("click", function () {
-      console.log("Switch clicked"); // Check if this logs
-      $(".toggle-btn").toggleClass("active");
+        console.log("Switch clicked"); // Check if this logs
+
+        $(".toggle-btn").toggleClass("active");
+
+        if ($(".toggle-btn").hasClass("active")) {
+            $("body").css("background-color", "#71b7d3"); // Unfocused
+        } else {
+            $("body").css("background-color", "#f0f4f8"); // Focused
+        }
     });
-  });
+});
+
   
