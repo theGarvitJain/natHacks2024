abfcbe189023ce35d7cbf51f5aa228681936242c
DamHanhNguyenMong <damhanhn@ualberta.ca>
2024-11-17 08:30:58 -0700

    scripts.js

--- flask_webapp/static/scripts.js
+++ flask_webapp/static/scripts.js
@@ -1,5 +1,5 @@
-// Javascript file 
 let timerInterval;
+let chartInstance;
 
 function updateTime() {
     fetch('/time')
@@ -20,9 +20,15 @@ function startTimer() {
 
 function stopTimer() {
     fetch('/stop', { method: 'POST' })
-        .then(() => {
+        .then(response => response.json())
+        .then(data => {
             clearInterval(timerInterval);
             timerInterval = null;
+
+            // Update graph data
+            if (data.data) {
+                updateGraph(data.data.focused, data.data.unfocused);
+            }
         });
 }
 
@@ -32,21 +38,54 @@ function resetTimer() {
             clearInterval(timerInterval);
             timerInterval = null;
             updateTime();
+
+            // Reset graph to empty values
+            updateGraph(0, 0);
         });
 }
 
-$(document).ready(function () {
-    $(".switch").on("click", function () {
-        console.log("Switch clicked"); // Check if this logs
+function loadGraph() {
+    const ctx = document.getElementById('sessionBarGraph').getContext('2d');
+    chartInstance = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Focused', 'Unfocused'],
+            datasets: [{
+                label: 'Session Data',
+                data: [0, 0], // Initially no data
+                backgroundColor: ['#007acc', '#c3e7f7']
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
 
+function updateGraph(focused, unfocused) {
+    chartInstance.data.datasets[0].data = [focused, unfocused];
+    chartInstance.update();
+}
+
+$(document).ready(function () {
+    // Load the graph on page load
+    loadGraph();
+
+    $(".switch").on("click", function () {
+        // Toggle the "active" class
         $(".toggle-btn").toggleClass("active");
 
+        // Play appropriate sound based on state
         if ($(".toggle-btn").hasClass("active")) {
-            $("body").css("background-color", "#71b7d3"); // Unfocused
+            $("body").css("background-color", "#71b7d3");
         } else {
-            $("body").css("background-color", "#f0f4f8"); // Focused
+            $("body").css("background-color", "#f0f4f8");
         }
     });
 });
-
-  
